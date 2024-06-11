7cd13656899aeaaefb9de96abaaaac0c5f7478c2
Max Haase <maxhaase@gmail.com>
2024-06-11 19:47:09 +0000

    Yeah

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Load variables from vars.env
+set -o allexport
 source vars.env
+set -o allexport-
 
 # Function to clean up sources.list
 clean_sources() {
@@ -73,56 +75,56 @@ version: '3.8'
 
 services:
   db_shared:
-    image: mysql:\${MYSQL_VERSION}
+    image: mysql:${MYSQL_VERSION}
     restart: always
     environment:
-      MYSQL_ROOT_PASSWORD: \${MYSQL_ROOT_PASSWORD}
-      MYSQL_DATABASE: \${MYSQL_DATABASE}
-      MYSQL_USER: \${MYSQL_USER}
-      MYSQL_PASSWORD: \${MYSQL_PASSWORD}
+      MYSQL_ROOT_PASSWORD: ${MYSQL_ROOT_PASSWORD}
+      MYSQL_DATABASE: ${MYSQL_DATABASE}
+      MYSQL_USER: ${MYSQL_USER}
+      MYSQL_PASSWORD: ${MYSQL_PASSWORD}
     volumes:
       - db_data:/var/lib/mysql
 
   wordpress1:
-    image: wordpress:\${WORDPRESS_VERSION}
+    image: wordpress:${WORDPRESS_VERSION}
     restart: always
     environment:
       WORDPRESS_DB_HOST: db_shared:3306
-      WORDPRESS_DB_USER: \${WORDPRESS_DB_USER}
-      WORDPRESS_DB_PASSWORD: \${WORDPRESS_DB_PASSWORD}
-      WORDPRESS_DB_NAME: \${MYSQL_DATABASE}
+      WORDPRESS_DB_USER: ${WORDPRESS_DB_USER}
+      WORDPRESS_DB_PASSWORD: ${WORDPRESS_DB_PASSWORD}
+      WORDPRESS_DB_NAME: ${MYSQL_DATABASE}
     depends_on:
       - db_shared
     ports:
-      - "\${DOMAIN1_PORT}:80"
+      - "${DOMAIN1_PORT}:80"
 
   wordpress2:
-    image: wordpress:\${WORDPRESS_VERSION}
+    image: wordpress:${WORDPRESS_VERSION}
     restart: always
     environment:
       WORDPRESS_DB_HOST: db_shared:3306
-      WORDPRESS_DB_USER: \${WORDPRESS_DB_USER}
-      WORDPRESS_DB_PASSWORD: \${WORDPRESS_DB_PASSWORD}
-      WORDPRESS_DB_NAME: \${MYSQL_DATABASE}
+      WORDPRESS_DB_USER: ${WORDPRESS_DB_USER}
+      WORDPRESS_DB_PASSWORD: ${WORDPRESS_DB_PASSWORD}
+      WORDPRESS_DB_NAME: ${MYSQL_DATABASE}
     depends_on:
       - db_shared
     ports:
-      - "\${DOMAIN2_PORT}:80"
+      - "${DOMAIN2_PORT}:80"
 
   mailserver:
-    image: tvial/docker-mailserver:\${MAILSERVER_VERSION}
+    image: tvial/docker-mailserver:${MAILSERVER_VERSION}
     hostname: mail
-    domainname: \${MAIL_DOMAIN}
+    domainname: ${MAIL_DOMAIN}
     restart: always
     env_file: vars.env
     ports:
-      - "\${SMTP_PORT}:25"
-      - "\${SMTPS_PORT}:465"
-      - "\${SMTP_ALT_PORT}:587"
-      - "\${POP3_PORT}:110"
-      - "\${POP3S_PORT}:995"
-      - "\${IMAP_PORT}:143"
-      - "\${IMAPS_PORT}:993"
+      - "${SMTP_PORT}:25"
+      - "${SMTPS_PORT}:465"
+      - "${SMTP_ALT_PORT}:587"
+      - "${POP3_PORT}:110"
+      - "${POP3S_PORT}:995"
+      - "${IMAP_PORT}:143"
+      - "${IMAPS_PORT}:993"
     volumes:
       - maildata:/var/mail
       - mailstate:/var/mail-state
@@ -134,13 +136,13 @@ services:
       ENABLE_POSTGREY: 0
 
   webmail:
-    image: roundcube/roundcubemail:\${ROUNDCUBEMAIL_VERSION}
+    image: roundcube/roundcubemail:${ROUNDCUBEMAIL_VERSION}
     restart: always
     environment:
-      ROUNDCUBEMAIL_DEFAULT_HOST: imaps://\${MAIL_DOMAIN}
-      ROUNDCUBEMAIL_SMTP_SERVER: tls://\${MAIL_DOMAIN}
+      ROUNDCUBEMAIL_DEFAULT_HOST: imaps://${MAIL_DOMAIN}
+      ROUNDCUBEMAIL_SMTP_SERVER: tls://${MAIL_DOMAIN}
     ports:
-      - "\${WEBMAIL_PORT}:80"
+      - "${WEBMAIL_PORT}:80"
     depends_on:
       - mailserver
 
@@ -149,12 +151,12 @@ services:
     restart: always
     environment:
       POSTFIXADMIN_DB_TYPE: mysqli
-      POSTFIXADMIN_DB_USER: \${MYSQL_USER}
-      POSTFIXADMIN_DB_PASS: \${MYSQL_PASSWORD}
+      POSTFIXADMIN_DB_USER: ${MYSQL_USER}
+      POSTFIXADMIN_DB_PASS: ${MYSQL_PASSWORD}
       POSTFIXADMIN_DB_HOST: db_shared
-      POSTFIXADMIN_DB_NAME: \${MYSQL_DATABASE}
-      POSTFIXADMIN_ADMIN_USER: \${POSTFIXADMIN_ADMIN_USER}
-      POSTFIXADMIN_ADMIN_PASS: \${POSTFIXADMIN_ADMIN_PASS}
+      POSTFIXADMIN_DB_NAME: ${MYSQL_DATABASE}
+      POSTFIXADMIN_ADMIN_USER: ${POSTFIXADMIN_ADMIN_USER}
+      POSTFIXADMIN_ADMIN_PASS: ${POSTFIXADMIN_ADMIN_PASS}
     depends_on:
       - db_shared
     ports:
